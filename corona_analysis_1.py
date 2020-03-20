beab51efd646061a961f0de0b991e46b97fb1991
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-03-20 13:10:07 -0300

    Create corona_analysis_1.py

---
+++ corona_analysis_1.py
@@ -0,0 +1,5 @@
+# From Kaggle:
+#   "Novel Corona Virus 2019 Dataset"
+#   "Day level information on covid-19 affected cases"
+#   https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset#covid_19_data.csv
+
