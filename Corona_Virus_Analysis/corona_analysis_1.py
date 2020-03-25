1718ba63d44349edafecabdb22bd8a381400cda0
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-03-24 23:42:39 -0300

    Update corona_analysis_1.py

--- Corona_Virus_Analysis/corona_analysis_1.py
+++ Corona_Virus_Analysis/corona_analysis_1.py
@@ -3,3 +3,7 @@
 #   "Day level information on covid-19 affected cases"
 #   https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset#covid_19_data.csv
 
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv("CSV_PATH.csv")
